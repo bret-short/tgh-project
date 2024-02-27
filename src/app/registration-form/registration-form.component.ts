38d76ec35c53604014f9b11eb652898782433fd0
Bret Short <bretstephenshort@live.com>
2024-02-27 14:17:25 -0700

    initial code commit

---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.css',
+})
+export class RegistrationFormComponent {
+  registerUser() {
+    console.log('User registered!');
+  }
+}
